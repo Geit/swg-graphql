f0e88b4e38979feacb3e5072dddc1e62a5b98a13
Rory King <rory_king@hotmail.co.uk>
2024-05-17 23:52:17 +0100

    fix: remove hardcoded API key

--- src/modules/transactions/config.ts
+++ src/modules/transactions/config.ts
@@ -1,2 +1,7 @@
+/**
+ * API key to use when doing self-queries of the GQL API via HTTP.
+ */
+export const TRANSACTIONS_GQL_API_KEY = process.env.TRANSACTIONS_GQL_API_KEY!;
+
 export const ELASTIC_SEARCH_TRANSACTION_INDEX_NAME =
   process.env.ELASTIC_SEARCH_TRANSACTION_INDEX_NAME ?? 'transaction-logging-alias';
--- src/modules/transactions/gqlSdk/index.ts
+++ src/modules/transactions/gqlSdk/index.ts
@@ -1,9 +1,11 @@
 import { GraphQLClient } from 'graphql-request';
 
+import { TRANSACTIONS_GQL_API_KEY } from '../config';
+
 import { getSdk } from './graphql.generated';
 
-const client = new GraphQLClient('http://omega01.swglegends.com:4000/graphql', {
-  headers: { Authorization: 'ApiKey-WhJqcJ9ENjVxVui3u5QdPTz2LZUZqHWX' },
+const client = new GraphQLClient('http://localhost:4000/graphql', {
+  headers: { Authorization: TRANSACTIONS_GQL_API_KEY },
   keepalive: true,
 });
 
