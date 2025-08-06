b04f9ad8e97bddff6930cdd1a8ae98f1609a920f
Rory King <rory_king@hotmail.co.uk>
2025-08-06 21:11:49 +0100

    Fixes for IFFReader

--- src/utils/IFFReader.ts
+++ src/utils/IFFReader.ts
@@ -19,14 +19,18 @@ interface FormStackType {
 
 interface ChunkStackType {
   type: number;
-  typeName: 'COLS';
+  typeName: string;
   size: number;
   data: Buffer;
 }
 
 type CombinedStackType = ChunkStackType | FormStackType;
 
-const iffParser = new Parser()
+const chunkParser = Parser.start().endianess('big').buffer('data', {
+  length: 'size',
+});
+
+const iffParser = Parser.start()
   .namely('formParser') //ree
   .useContextVars()
   .array('children', {
@@ -38,18 +42,34 @@ const iffParser = new Parser()
       .choice('', {
         tag: 'type',
         choices: {
-          [TAGIFY('FORM')]: new Parser()
+          [TAGIFY('FORM')]: Parser.start()
             .uint32('subType')
             .seek(-4)
             .string('subTypeName', { encoding: 'utf-8', length: 4 })
             .nest('formData', { type: 'formParser' }),
         },
         // Otherwise it's a chunk
-        defaultChoice: Parser.start().endianess('big').buffer('data', {
-          length: 'size',
-        }),
+        defaultChoice: chunkParser,
       }),
-    readUntil: 'eof',
+    readUntil(item, buffer) {
+      // @ts-expect-error type this later
+      if (this.$parent && this.$parent.type === TAGIFY('FORM')) {
+        const alreadyReadSize =
+          // @ts-expect-error type this later
+          this.children?.reduce(
+            (acc: number, cur: { size: number; type: number }) =>
+              acc + cur.size + (cur.type === TAGIFY('FORM') ? 12 : 8),
+            // Start with 4 to account for FORM subtype
+            4
+          ) ?? 0;
+
+        // @ts-expect-error type this later
+        return alreadyReadSize >= this.$parent.size;
+      }
+      if (buffer.length === 0) return true; // No buffer left to read;
+
+      return false;
+    },
   });
 
 export class IffReader {
@@ -57,15 +77,16 @@ export class IffReader {
 
   rootStack: FormStackType;
 
-  currentStack: CombinedStackType[] | ChunkStackType;
-  previousStack: CombinedStackType[] | ChunkStackType;
+  currentNode: CombinedStackType[] | ChunkStackType;
+  stack: (CombinedStackType[] | ChunkStackType)[];
 
   constructor(data: Buffer) {
     const iffRoot: IffRootType = iffParser.parse(data);
 
     this.parsedData = iffRoot;
 
-    this.currentStack = this.parsedData.children;
+    this.currentNode = this.parsedData.children;
+    this.stack = [];
   }
 
   private static isForm(stack: CombinedStackType): stack is FormStackType {
@@ -76,43 +97,57 @@ export class IffReader {
     return stack.type !== TAGIFY('FORM');
   }
 
-  enterForm(name: string) {
-    const previousStack = this.currentStack;
+  getFormNameCount(name: string) {
+    if (!Array.isArray(this.currentNode)) throw new Error('A form cannot be a child of a chunk!');
 
-    if (!Array.isArray(this.currentStack)) throw new Error('A form cannot be a child of a chunk!');
+    return this.currentNode.filter(n => IffReader.isForm(n) && n.subTypeName === name).length;
+  }
 
-    const newForm = this.currentStack.filter(IffReader.isForm).find(node => node.subTypeName === name);
+  hasForm(name: string) {
+    if (!Array.isArray(this.currentNode)) throw new Error('A form cannot be a child of a chunk!');
+
+    return this.currentNode.filter(n => IffReader.isForm(n) && n.subTypeName === name).length > 0;
+  }
+
+  enterForm(name: string, idx = 0) {
+    if (!Array.isArray(this.currentNode)) throw new Error('A form cannot be a child of a chunk!');
+
+    const newForm = this.currentNode
+      .filter(IffReader.isForm)
+      .filter(node => node.subTypeName === name)
+      .at(idx);
 
     if (!newForm) throw new Error(`No form named ${name} found!`);
 
-    this.currentStack = newForm.formData.children;
-    this.previousStack = previousStack;
+    this.stack.push(this.currentNode);
+    this.currentNode = newForm.formData.children;
   }
 
   exitForm() {
-    this.currentStack = this.previousStack;
+    this.currentNode = this.stack.pop()!;
   }
 
-  enterChunk(name: string) {
-    const previousStack = this.currentStack;
+  enterChunk(name: string, idx = 0) {
+    if (!Array.isArray(this.currentNode)) throw new Error('Already in a chunk!');
 
-    if (!Array.isArray(this.currentStack)) throw new Error('Already in a chunk!');
-
-    const newChunk = this.currentStack.filter(IffReader.isChunk).find(node => node.typeName === name);
+    const newChunk = this.currentNode
+      .filter(IffReader.isChunk)
+      .filter(node => node.typeName === name)
+      .at(idx);
 
     if (!newChunk) throw new Error(`No chunk named ${name} found!`);
 
-    this.currentStack = newChunk;
-    this.previousStack = previousStack;
+    this.stack.push(this.currentNode);
+    this.currentNode = newChunk;
   }
 
   exitChunk() {
-    this.currentStack = this.previousStack;
+    this.currentNode = this.stack.pop()!;
   }
 
   getChunkData(): Buffer {
-    if (Array.isArray(this.currentStack)) throw new Error('Not currently in a chunk!');
+    if (Array.isArray(this.currentNode)) throw new Error('Not currently in a chunk!');
 
-    return this.currentStack.data;
+    return this.currentNode.data;
   }
 }
