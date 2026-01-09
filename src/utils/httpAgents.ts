4d5352abd5e721cb22bcf886373655c68be4666e
Rory King <rory_king@hotmail.co.uk>
2026-01-09 00:27:15 +0000

    Migrate away from Axios

--- src/utils/httpAgents.ts
+++
@@ -1,5 +0,0 @@
-import http from 'http';
-import https from 'https';
-
-export const httpAgent = new http.Agent({ keepAlive: true });
-export const httpsAgent = new https.Agent({ keepAlive: true });
